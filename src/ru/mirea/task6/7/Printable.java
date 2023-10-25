77fcaf3d6c70e5ed66de52ca701cf5a0a3a95ff1
Alexandr Komissarov <komissarov1694@yandex.ru>
2023-10-25 20:52:56 +0300

    Create Printable.java

---
+++ src/ru/mirea/task6/7/Printable.java
@@ -0,0 +1,46 @@
+package ru.mirea.task6;
+
+import  java.util.Objects;
+
+public interface Printable {
+    void print();
+}
+
+class Book implements Printable {
+    private String name;
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Printing book " + getName());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(name, book.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "name='" + name + '\'' + '}';
+    }
+}
