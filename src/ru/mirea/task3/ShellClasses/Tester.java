b9f1b0eb0928fecd746fe15e72ca661af14567b2
Alexandr Komissarov <komissarov1694@yandex.ru>
2023-12-22 15:59:21 +0300

    Create Tester.java

---
+++ src/ru/mirea/task3/ShellClasses/Tester.java
@@ -0,0 +1,14 @@
+package ru.mirea.task3;
+
+public class Tester {
+    public static void main(String[] args) {
+        String a = "123.456";
+        Double b = Double.valueOf(a);
+        Double c = Double.parseDouble(a);
+        System.out.println(b);
+        System.out.println(c);
+        Double d = 121.1;
+
+
+    }
+}
